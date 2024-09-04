60cd856f8e98430b693c275942ea065f5c0fea8b
modbender <{ID}+{username}@users.noreply.github.com>
2024-09-04 10:52:50 +0530

    feat: upgrade packages

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,6 +2,6 @@ import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   modules: ["../src/module"],
-  
   devtools: { enabled: process.env.NODE_ENV === "development" },
+  compatibilityDate: "2024-09-04",
 });
